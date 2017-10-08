ee6ceb479b3539d00d8e5b6244c2e50fea7015cc
Timothy Cevallos <timcev@gmail.com>
2017-10-08 20:20:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function createPost(){
+  var pageTemplate = _.template(document.getElementById(`page-template`).innerHTML);
+  var postTemplate = _.template(document.getElementById(`post-template`).innerHTML);
+  var commentsTemplate = _.template(document.getElementById('comments-template').innerHTML);
+  var postTitle = document.getElementById("postTitle").value;
+  var postAuthor = document.getElementById('postAuthor').value;
+  var post = document.getElementById("postbody").value;
+
+}
+
+function postComment(){
+
+}
