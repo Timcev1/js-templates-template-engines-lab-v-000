5ec745a4ba96cd48fc24527472973cf2f99b11c2
Timothy Cevallos <timcev@gmail.com>
2017-10-08 21:01:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function createPost(){
   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
-  var pageTemplate = _.template(document.getElementById(`page-template`).innerHTML);
-  var postTemplate = _.template(document.getElementById(`post-template`).innerHTML);
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
   var commentsTemplate = _.template(document.getElementById('comments-template').innerHTML);
   var postTitle = document.getElementById("postTitle").value;
   var postAuthor = document.getElementById('postAuthor').value;
